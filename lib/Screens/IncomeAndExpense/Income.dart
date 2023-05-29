1e496374a64d269910aedec4797b9f0e8f4bf874
Yash <yv48183@gmail.com>
2023-05-29 15:41:07 +0530

    All files

--- lib/Screens/IncomeAndExpense/Income.dart
+++ lib/Screens/IncomeAndExpense/Income.dart
@@ -1,5 +1,4 @@
 import 'package:financeapk/Screens/DatabaseMethods/DatabaseHelper.dart';
-import 'package:financeapk/Screens/IncomeAndExpense/UpdateIncome.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get/get_core/src/get_main.dart';
@@ -13,13 +12,36 @@ class Income extends StatefulWidget {
 }
 
 class _IncomeState extends State<Income> {
+  GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  TextEditingController _NameController = TextEditingController();
+
+  TextEditingController _SourceController = TextEditingController();
+
+  TextEditingController _CategoryController = TextEditingController();
+
+  TextEditingController _AmountController = TextEditingController();
+
+  TextEditingController _DateController = TextEditingController();
+
+  final FocusNode _NameNode = FocusNode();
+
+  final FocusNode _SourceNode = FocusNode();
+
+  final FocusNode _CategoryNode = FocusNode();
+
+  final FocusNode _AmountNode = FocusNode();
+
+  final FocusNode _DateNode = FocusNode();
+
+  double deviceHeight(BuildContext context) => MediaQuery.of(context).size.height;
+
+  double deviceWidth(BuildContext context) => MediaQuery.of(context).size.width;
   void initState(){
     super.initState();
     _refreshJournals();
     print("...number of items ${_journals.length}");
   }
-  double deviceHeight(BuildContext context) => MediaQuery.of(context).size.height;
-  double deviceWidth(BuildContext context) => MediaQuery.of(context).size.width;
 
   List<Map<String,dynamic>> _journals= [];
   bool _isloading=true;
@@ -63,7 +85,212 @@ class _IncomeState extends State<Income> {
                   child: Row(
                     children: [
                       IconButton(onPressed: (){
-                        Get.to(UpdateItem(),arguments: [{'id': _journals[index]['id']}]);
+                        showModalBottomSheet(
+                            context: context,
+                            isScrollControlled: true,
+                            shape: const RoundedRectangleBorder(
+                              borderRadius: BorderRadius.vertical(
+                                top: Radius.circular(25.0),
+                              ),
+                            ),
+                            enableDrag: true,
+                            // backgroundColor: Colors.orangeAccent[100],
+                            builder: (context) {
+                              return Container(
+                                height: 650,
+                                padding: EdgeInsets.fromLTRB(50.0, 30.0, 50.0, 10.0),
+                                child: Container(
+                                  child: SingleChildScrollView(
+                                    child: Form(
+                                      key: _formKey,
+                                      child: Container(
+                                        width: deviceWidth(context)*0.9,
+                                        margin: EdgeInsets.only(top: deviceHeight(context)*0.09, right:deviceWidth(context)*0.05,left: deviceWidth(context)*0.05),
+                                        child: Column(
+                                          mainAxisSize: MainAxisSize.min,
+                                          mainAxisAlignment: MainAxisAlignment.start,
+                                          children: [
+                                            Container(
+                                              //margin: EdgeInsets.only(top: deviceHeight(context)*0.09, deviceWidth(context)*0.05,left: deviceWidth(context)*0.05),
+                                              child: SizedBox(
+                                                width: deviceWidth(context),
+                                                child: TextField(
+                                                  controller: _NameController,
+                                                  obscureText: false,
+                                                  focusNode: _NameNode,
+                                                  keyboardType: TextInputType.name,
+                                                  decoration: InputDecoration(
+                                                    enabledBorder: OutlineInputBorder(
+                                                      borderSide: BorderSide(
+                                                        width: 0,
+                                                      ),
+                                                      borderRadius:
+                                                      BorderRadius.circular(15.0),
+                                                    ),
+                                                    focusedBorder: OutlineInputBorder(
+                                                      borderSide: BorderSide(
+                                                          width: 5,
+                                                          color:
+                                                          Colors.red.shade500),
+                                                      borderRadius:
+                                                      BorderRadius.circular(15.0),
+                                                    ),
+                                                    hintText: 'Enter Your name',
+                                                  ),
+                                                ),
+                                              ),
+                                            ),
+                                            Container(
+                                              margin: EdgeInsets.only(top: deviceHeight(context)*0.03,),
+                                              child: SizedBox(
+                                                width: deviceWidth(context),
+                                                child: TextField(
+                                                  controller: _SourceController,
+                                                  obscureText: false,
+                                                  focusNode: _SourceNode ,
+                                                  keyboardType: TextInputType.name,
+                                                  decoration: InputDecoration(
+                                                    enabledBorder: OutlineInputBorder(
+                                                      borderSide: BorderSide(
+                                                        width: 0,
+                                                      ),
+                                                      borderRadius:
+                                                      BorderRadius.circular(15.0),
+                                                    ),
+                                                    focusedBorder: OutlineInputBorder(
+                                                      borderSide: BorderSide(
+                                                          width: 5,
+                                                          color:
+                                                          Colors.red.shade500),
+                                                      borderRadius:
+                                                      BorderRadius.circular(15.0),
+                                                    ),
+                                                    hintText: 'Enter Your Source Of income',
+                                                  ),
+                                                ),
+                                              ),
+                                            ),
+                                            Container(
+                                              margin: EdgeInsets.only(top: deviceHeight(context)*0.03,),
+                                              child: SizedBox(
+                                                width: deviceWidth(context),
+                                                child: TextField(
+                                                  controller: _CategoryController,
+                                                  obscureText: false,
+                                                  focusNode: _CategoryNode ,
+                                                  keyboardType: TextInputType.name,
+                                                  decoration: InputDecoration(
+                                                    enabledBorder: OutlineInputBorder(
+                                                      borderSide: BorderSide(
+                                                        width: 0,
+                                                      ),
+                                                      borderRadius:
+                                                      BorderRadius.circular(15.0),
+                                                    ),
+                                                    focusedBorder: OutlineInputBorder(
+                                                      borderSide: BorderSide(
+                                                          width: 5,
+                                                          color:
+                                                          Colors.red.shade500),
+                                                      borderRadius:
+                                                      BorderRadius.circular(15.0),
+                                                    ),
+                                                    hintText: 'Enter Category(e.g., salary, gifts, rent, food, transport, etc.)',
+                                                  ),
+                                                ),
+                                              ),
+                                            ),
+                                            Container(
+                                              margin: EdgeInsets.only(top: deviceHeight(context)*0.03,),
+                                              child: SizedBox(
+                                                width: deviceWidth(context),
+                                                child: TextField(
+                                                  controller: _AmountController ,
+                                                  obscureText: false,
+                                                  focusNode: _AmountNode,
+                                                  keyboardType: TextInputType.number,
+                                                  decoration: InputDecoration(
+                                                    enabledBorder: OutlineInputBorder(
+                                                      borderSide: BorderSide(
+                                                        width: 0,
+                                                      ),
+                                                      borderRadius:
+                                                      BorderRadius.circular(15.0),
+                                                    ),
+                                                    focusedBorder: OutlineInputBorder(
+                                                      borderSide: BorderSide(
+                                                          width: 5,
+                                                          color:
+                                                          Colors.red.shade500),
+                                                      borderRadius:
+                                                      BorderRadius.circular(15.0),
+                                                    ),
+                                                    hintText: 'Enter Your Amount',
+                                                  ),
+                                                ),
+                                              ),
+                                            ),
+                                            Container(
+                                              margin: EdgeInsets.only(top: deviceHeight(context)*0.03,),
+                                              child: SizedBox(
+                                                width: deviceWidth(context),
+                                                child: TextField(
+                                                  controller: _DateController ,
+                                                  obscureText: false,
+                                                  focusNode: _DateNode ,
+                                                  keyboardType: TextInputType.datetime,
+                                                  decoration: InputDecoration(
+                                                    enabledBorder: OutlineInputBorder(
+                                                      borderSide: BorderSide(
+                                                        width: 0,
+                                                      ),
+                                                      borderRadius:
+                                                      BorderRadius.circular(15.0),
+                                                    ),
+                                                    focusedBorder: OutlineInputBorder(
+                                                      borderSide: BorderSide(
+                                                          width: 5,
+                                                          color:
+                                                          Colors.red.shade500),
+                                                      borderRadius:
+                                                      BorderRadius.circular(15.0),
+                                                    ),
+                                                    hintText: 'Enter Your Date',
+                                                  ),
+                                                ),
+                                              ),
+                                            ),
+                                            Container(
+                                              margin: EdgeInsets.only(top: deviceHeight(context)*0.05,),
+                                              child: SizedBox(
+                                                width: deviceWidth(context)*0.9,
+                                                height: deviceHeight(context)*0.1,
+                                                child: ElevatedButton(
+                                                  onPressed: () async{
+                                                     await SQLHelper.updateIncome(_journals[index]['id'],_NameController.text, _SourceController.text, _CategoryController.text, _AmountController.text, _DateController.text);
+                                                     _refreshJournals();
+                                                  },
+                                                  child: Text("Update Income"),
+                                                  style: ElevatedButton.styleFrom(
+                                                    shape: RoundedRectangleBorder(
+                                                        borderRadius: BorderRadius.circular(20)
+                                                    ),
+                                                    backgroundColor: Colors.redAccent,
+
+                                                  ),
+
+                                                ),
+                                              ),
+                                            ),
+                                          ],
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              );
+                            });
+
                       }, icon: const Icon(Icons.edit))
                     ],
                   ),
